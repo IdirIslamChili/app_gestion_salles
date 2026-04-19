6cf790196c1c476490010ee910aac8a2054aa4fb
Idir Islam Chili <chiliidirislam@gmail.com>
2026-04-18 21:35:51 -0400

    tester le bon fonctionnement des méthodes de la classe

--- main.py
+++ main.py
@@ -1,6 +1,44 @@
 from models.salle import Salle
 from data.dao_salle import DataSalle
 
-data_user = DataSalle()
-con = data_user.get_connection()
-print(con)
+#connexion à la base de données
+data_salle = DataSalle()
+connexion = data_salle.get_connection()
+print(connexion)
+
+#Création des salles
+sl1=Salle("C250","INF1010", "Laboratoire", 40)
+sl2=Salle("C260","INF1011", "Classe", 38)
+sl3=Salle("C240","ANG1011", "Classe", 22)
+sl4=Salle("C210","MAT1011", "Classe", 25)
+
+#Ajout des salles
+dao = DataSalle()
+dao.insert_salle(sl1)
+dao.insert_salle(sl2)
+dao.insert_salle(sl3)
+dao.insert_salle(sl4)
+
+#Suppression d'une salle avec son code
+dao.delete_salle("C260")
+
+#Modifier une salle
+sl1_modifie = Salle("INF1010 Modifié", "Laboratoire", 45, "C250")
+dao.update_salle(sl1_modifie)
+
+#Rechercher d'une salle par son code
+salle = dao.get_salle("C250")
+if salle:
+    salle.afficher_info()
+else:
+    print("Salle introuvable")
+
+#Récupération et affichage de toutes les salles
+salles = dao.get_salles()
+if salles:
+    print("Liste des salles :")
+    for salle in salles:
+        salle.afficher_info()
+        print("-------------------")
+else:
+    print("Aucune salle trouvée")
