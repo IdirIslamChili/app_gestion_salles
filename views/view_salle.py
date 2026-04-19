e36ddd3717af1bcf93bd3b8f54e643b4f9a822f2
Idir Islam Chili <chiliidirislam@gmail.com>
2026-04-19 11:28:44 -0400

    Implémentation du cadre Informations salle et l'ajout des labels et Entry

--- views/view_salle.py
+++ views/view_salle.py
@@ -4,3 +4,25 @@ class ViewSalle (ctk.CTk):
     def __init__(self):
         super().__init__()
         self.service_salle = ServiceSalle()
+
+        self.title("Gestion des salles")
+        self.geometry("700x500")
+        ctk.set_appearance_mode("dark")
+        self.frame_info = ctk.CTkFrame(self)
+        self.frame_info.pack(padx=10, pady=10, fill="x")
+
+        ctk.CTkLabel(self.frame_info, text="Code salle").grid(row=0, column=0, padx=5, pady=5)
+        self.entry_code = ctk.CTkEntry(self.frame_info)
+        self.entry_code.grid(row=0, column=1, padx=5, pady=5)
+
+        ctk.CTkLabel(self.frame_info, text="Libellé").grid(row=1, column=0, padx=5, pady=5)
+        self.entry_libelle = ctk.CTkEntry(self.frame_info)
+        self.entry_libelle.grid(row=1, column=1, padx=5, pady=5)
+
+        ctk.CTkLabel(self.frame_info, text="Type").grid(row=2, column=0, padx=5, pady=5)
+        self.entry_type = ctk.CTkEntry(self.frame_info)
+        self.entry_type.grid(row=2, column=1, padx=5, pady=5)
+
+        ctk.CTkLabel(self.frame_info, text="Capacité").grid(row=3, column=0, padx=5, pady=5)
+        self.entry_capacite = ctk.CTkEntry(self.frame_info)
+        self.entry_capacite.grid(row=3, column=1, padx=5, pady=5)
