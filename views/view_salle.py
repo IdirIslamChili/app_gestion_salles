354150cf4c0c515ebc25711973dfb2ef6ded76f8
Idir Islam Chili <chiliidirislam@gmail.com>
2026-04-19 14:35:37 -0400

    Ajout de la méthode lister_salles

--- views/view_salle.py
+++ views/view_salle.py
@@ -101,3 +101,13 @@ class ViewSalle (ctk.CTk):
 
             self.entry_capacite.delete(0, "end")
             self.entry_capacite.insert(0, salle.capacite)
+
+    def lister_salles(self):
+        self.treeList.delete(*self.treeList.get_children())
+        liste = self.service_salle.recuperer_salles()
+        for s in liste:
+            self.treeList.insert(
+                "",
+                "end",
+                values=(s.code, s.libelle, s.type_salle, s.capacite)
+            )
