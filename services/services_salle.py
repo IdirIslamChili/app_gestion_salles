9d85cf5a53a20c690140b4a923ae067f3d7a856b
Idir Islam Chili <chiliidirislam@gmail.com>
2026-04-18 22:36:13 -0400

    Ajout de la méthode modifier_salle

--- services/services_salle.py
+++ services/services_salle.py
@@ -13,3 +13,13 @@ class ServiceSalle:
 
         self.dao_salle.insert_salle(salle)
         return True, "Salle ajoutée avec succès"
+
+    def  modifier_salle(self, salle):
+        if not salle.code or not salle.libelle or not salle.type or salle.capacite is None:
+            return False, "Tous les champs doivent être remplis"
+
+        if salle.capacite < 1:
+            return False, "Capacité invalide"
+
+        self.dao_salle.update_salle(salle)
+        return True, "Salle modifiée avec succès"
