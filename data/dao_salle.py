15283e9ca382aa0a6a5cd242995009bcbf7df789
Idir Islam Chili <chiliidirislam@gmail.com>
2026-04-18 20:05:21 -0400

    Création de la classe DataSalle et l'insertion des méthodes

---
+++ data/dao_salle.py
@@ -0,0 +1,85 @@
+import json
+import mysql.connector
+from models.salle import Salle
+class DataSalle:
+    def __init__(self):
+        pass
+    def get_connection(self):
+        with open ("./data/config.json", "r", encoding="utf-8") as f:
+            config = json.load(f)
+        connexion = mysql.connector.connect(
+            host=config["host"],
+            user=config["user"],
+            password=config["password"],
+            database=config["database"]
+        )
+        return connexion
+
+    def insert_salle(self, salle):
+        connexion = self.get_connection()
+        crs = connexion.cursor()
+        crs.execute(
+            "INSERT INTO utilisateurs VALUES(%s,%s,%s,%s)",
+            (salle.code, salle.libelle, salle.type, salle.capacite)
+        )
+        connexion.commit()
+        crs.close()
+        connexion.close()
+
+    def update_salle(self, salle):
+        connexion = self.get_connection()
+        crs = connexion.cursor()
+        crs.execute(
+            """
+                UPDATE salle
+                SET libelle = %s,
+                    type = %s,
+                    capacite = %s
+                WHERE code = %s
+            """,
+            (salle.code, salle.libelle, salle.type, salle.capacite)
+            )
+        connexion.commit()
+        crs.close()
+        connexion.close()
+
+    def delete_salle(self, code):
+        connexion = self.get_connection()
+        crs = connexion.cursor()
+        crs.execute(" DELETE FROM salle WHERE code = %", (code)
+        )
+        connexion.commit()
+        crs.close()
+        connexion.close()
+
+    def get_salle(self, code):
+        connexion = self.get_connection()
+        crs = connexion.cursor()
+        crs.execute("SELECT * FROM salle WHERE code = %", (code))
+
+        resultat = crs.fetchone()
+
+        connexion.commit()
+        crs.close()
+        connexion.close()
+
+        if resultat:
+            return Salle(*resultat)
+        else:
+            return None
+
+    def get_salles(self):
+        connexion = self.get_connection()
+        crs = connexion.cursor()
+        crs.execute("SELECT * FROM salle")
+        resultat = crs.fetchall()
+
+        salles = []
+        for row in resultat:
+            salles.append(Salle(*row))
+
+        connexion.commit()
+        crs.close()
+        connexion.close()
+
+        return salles
