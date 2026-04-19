8eb065ee459a929ef3815890aaea53367e505b99
Idir Islam Chili <chiliidirislam@gmail.com>
2026-04-18 22:40:40 -0400

    Ajout de la méthode rechercher_salle

--- services/services_salle.py
+++ services/services_salle.py
@@ -7,10 +7,8 @@ class ServiceSalle:
     def ajouter_salle(self, salle):
         if not salle.code or not salle.libelle or not salle.type or salle.capacite is None:
             return False, "Tous les champs doivent être remplis"
-
         if salle.capacite < 1:
             return False, "La capacité doit être >= 1"
-
         self.dao_salle.insert_salle(salle)
         return True, "Salle ajoutée avec succès"
 
@@ -20,13 +18,17 @@ class ServiceSalle:
 
         if salle.capacite < 1:
             return False, "Capacité invalide"
-
         self.dao_salle.update_salle(salle)
         return True, "Salle modifiée avec succès"
 
     def supprimer_salle(self, code):
         if not code:
             return False, "Code invalide"
-
         self.dao_salle.delete_salle(code)
         return True, "Salle supprimée"
+
+    def rechercher_salle(self, code):
+        if not code:
+            return  False, "Code invalide"
+        self.dao_salle.get_salle (code)
+
